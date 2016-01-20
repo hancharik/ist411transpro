08e83cf42ba0e336fe6e03f1bb3f926244decf3e
cboucher13 <Caroline@104.39.124.157>
2016-01-19 19:50:05 -0500

    hi

--- src/orderprocessingsystem/OrderProcessingSystem.java
+++ src/orderprocessingsystem/OrderProcessingSystem.java
@@ -15,7 +15,7 @@ public class OrderProcessingSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // yadda yadda yadda
+        // this is annoying
     }
     
 }
